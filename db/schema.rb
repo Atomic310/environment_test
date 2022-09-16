9d9f4f5a076774b58a4015d315c8b681eaa04472
atomic310 <atomic310@tamu.edu>
2022-09-15 23:07:33 -0500

    your commit message

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,8 @@ ActiveRecord::Schema.define(version: 2022_08_26_183601) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
+	t.string "author"
+	t.numeric "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
